0951665b61c5e9518c2d9376878bfb64d04e425a
atharvak031 <atharva@clingmultisolutions.org>
2024-09-13 13:43:26 +0530

    Adding new files

---
+++ src/MultidimensionalArrays.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MultidimensionalArrays
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+
+        // Initialize inner ArrayLists and add them to the outer ArrayList
+        for (int i = 0; i < 5; i++) {
+            ArrayList<Integer> innerList = new ArrayList<>();
+            for (int j = 0; j < 5; j++) {
+                innerList.add(sc.nextInt());
+            }
+            list.add(innerList);
+        }
+
+        System.out.println(list);
+    }
+}
