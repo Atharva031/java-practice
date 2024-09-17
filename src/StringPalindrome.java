d3e6b3d01ccf600b1d72dd44ff62378eaa17143f
atharvak031 <atharva.kulkarni031@gmail.com>
2024-09-17 11:39:17 +0530

    Adding new files

---
+++ src/StringPalindrome.java
@@ -0,0 +1,26 @@
+public class StringPalindrome {
+    public static boolean isPalindrome(String str) {
+        String cleanedStr = str.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
+        int left = 0;
+        int right = cleanedStr.length() - 1;
+
+        while (left < right) {
+            if (cleanedStr.charAt(left) != cleanedStr.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String input = "tenet";
+        if (isPalindrome(input)) {
+            System.out.println("\"" + input + "\" is a palindrome");
+        } else {
+            System.out.println("\"" + input + "\" is not a palindrome");
+        }
+    }
+}
