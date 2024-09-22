387f05e2f3d645e81b197fd686bf5958423229d3
atharvak031 <atharva.kulkarni031@gmail.com>
2024-09-22 22:21:40 +0530

    Adding new files

--- src/LearnCollectionsClass.java
+++ src/LearnCollectionsClass.java
@@ -1,5 +1,7 @@
+import java.sql.Array;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class LearnCollectionsClass
@@ -17,6 +19,35 @@ public class LearnCollectionsClass
         System.out.println("min element: " + Collections.min(list));
         System.out.println("max element: " + Collections.max(list));
         System.out.println("frequency of element: " + Collections.frequency(list,9));
+
+        Collections.sort(list);
+        System.out.println(list);
+
+
+        List<Student> list2 = new ArrayList<>();
+        list2.add(new Student("Atharva", 1));
+        list2.add(new Student("Dattatray", 5));
+        list2.add(new Student("Abhay", 2));
+        list2.add(new Student("Varsha", 4));
+        list2.add(new Student("Abhishek", 3));
+        list2.add(new Student("Prabhavati", 6));
+
+        Student s1 = new Student("Aakash", 7);
+        Student s2 = new Student("Nishad", 8);
+
+        System.out.println(list2);
+//        System.out.println((s1.compareTo(s2)));
+        Collections.sort(list2, new Comparator<Student>() {
+            @Override
+            public int compare(Student o1, Student o2) {
+                return o1.name.compareTo(o2.name);
+            }
+        });
+        System.out.println(list2);
+
+
+        Collections.sort(list2);
+
 
     }
 }
