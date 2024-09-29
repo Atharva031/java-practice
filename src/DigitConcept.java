7406f3cc060f856ca499cb31bf9b363d760e6694
atharvak031 <atharva.kulkarni031@gmail.com>
2024-09-29 10:36:25 +0530

    Adding new files

--- src/DigitConcept.java
+++ src/DigitConcept.java
@@ -1,4 +1,10 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
+
+import static java.lang.Math.sqrt;
+import static java.util.Collections.sort;
 
 public class DigitConcept
 {
@@ -47,7 +53,70 @@ public class DigitConcept
 
     static void checkArmstrong(int n)
     {
+        int sum = 0;
+        int duplicateN = n;
+        while(n>0)
+        {
+            int lastNumber = n%10;
+            sum = sum + (lastNumber*lastNumber*lastNumber);
+            n = n/10;
+        }
+        if(sum == duplicateN)
+        {
+            System.out.println("It is an Armstrong Number");
+        }
+        else
+        {
+            System.out.println("It is not an Armstrong Number");
+        }
+    }
 
+    static void printAllDivisors(int n)
+    {
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 1; i*i<=n; i++)
+        {
+            if (n % i == 0)
+            {
+                list.add(i);
+                if (n / i != i)
+                {
+                    list.add(n / i);
+                }
+            }
+        }
+
+        Collections.sort(list);
+
+        for (Integer num : list)
+        {
+            System.out.println(num);
+        }
+    }
+
+    static void checkPrime(int n)
+    {
+        int counter = 0;
+        for(int i = 1; i*i<=n; i++)
+        {
+            if(n%i==0)
+            {
+                counter++;
+                if(n/i != i)
+                {
+                    counter++;
+                }
+            }
+        }
+        if(counter==2)
+        {
+            System.out.println("It is a prime number");
+        }
+        else
+        {
+            System.out.println("It is not a prime number");
+        }
     }
 
     public static void main(String[] args)
@@ -56,6 +125,9 @@ public class DigitConcept
         int n = sc.nextInt();
 //        countDigits(n);
 //        ReverseTheDigits(n);
-        checkPalindrome(n);
+//        checkPalindrome(n);
+//        checkArmstrong(n);
+//        printAllDivisors(n);
+        checkPrime(n);
     }
 }
